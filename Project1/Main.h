ef0485f9ef5a42e608f4ee056da801000b43d227
manish <manish chauhan>
2021-11-20 21:39:26 +0530

    updated code

---
+++ Project1/Main.h
@@ -0,0 +1,42 @@
+#pragma once
+#include<iostream>
+#include "PoolManager.h"
+#include<memory>
+class SomeSprite
+{
+private:
+	const std::string name="some random sprite";
+public:
+	SomeSprite() {};
+	~SomeSprite() {};
+	const std::string& getName() const
+	{
+		return name;
+	}
+};
+int main()
+{
+	/*
+		delcare a pool manager
+	*/
+	std::unique_ptr<PoolManager<SomeSprite>> pool = std::make_unique<PoolManager<SomeSprite>>();
+	//add 75 sprites
+	for (int i = 0; i < 75; i++)
+	{
+		SomeSprite* sp = new SomeSprite();
+		pool->addSprite(sp);
+	}
+
+	std::vector<SomeSprite*> random_pool;
+	for (int i = 0; i < 10; i++)
+	{
+		SomeSprite* sp = new SomeSprite();
+		random_pool.push_back(sp);
+	}
+	pool->addToPool(random_pool);
+	for (auto sprite :pool->getSprites(84))
+	{
+		std::cout << sprite->getName() << std::endl;
+	}
+	return 0;
+}
---
+++ Project1/PoolManager.h
@@ -0,0 +1,91 @@
+#pragma once
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<mutex>
+/*
+	Pool Manager class
+*/
+
+template<typename T>
+class PoolManager
+{
+	private:
+		/*
+			pool can only hold 100 sprites
+		*/
+		std::mutex mt;
+		const int Limit = 100;
+		std::deque<T*> poolContainer;
+	public:
+		PoolManager() {};
+		~PoolManager() {};
+		void addToPool(std::vector<T*> sprites)
+		{
+			if (sprites.size() >= Limit)
+			{
+				std::cout << "too many spries in the pool" << std::endl;
+				return;
+			}
+			std::unique_lock<std::mutex> lock(mt);
+			for (auto sprite:sprites)
+			{
+				poolContainer.push_back(sprite);
+			}
+		};
+		/*
+			get multiple sprites from pool
+		*/
+
+		std::vector<T*> getSprites(int total)
+		{
+			std::vector<T*> sprites;
+			if (total >= poolContainer.size())
+			{
+				std::cout << "sprite pool don't contain that many sprites" << std::endl;
+				return sprites;
+			}
+			std::unique_lock<std::mutex> lock(mt);
+		
+			for (int i = 0; i < total; i++)
+			{
+				sprites.push_back(poolContainer.front());
+			}
+			return sprites;
+		}
+		/*
+			Destory pool
+		*/
+		void DestoryPool()
+		{
+			for (auto sprite : poolContainer)
+			{
+				delete sprite;
+			}
+			poolContainer.clear();
+		}
+		/*
+			Get Single sprite from the pool
+		*/
+		T* getSprite()
+		{
+			if (poolContainer.size()>0)
+			{
+				return poolContainer.pop();
+			}
+			return nullptr;
+		}
+		/*
+		  add single sprite in pool
+		*/
+		void addSprite(T *sprite)
+		{
+			if (Limit <= poolContainer.size())
+			{
+				std::cout << "adding not allowed to pool" << std::endl;
+				return;
+			}
+			poolContainer.push_back(sprite);
+		}
+};
+
